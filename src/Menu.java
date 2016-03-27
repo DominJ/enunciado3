86cd1db862d13b663c27426837d1a3130a9680f8
domin <domin@192.168.1.33>
2016-03-27 12:21:36 +0200

    Comentarios modificados. TODO añadidos.

--- src/Menu.java
+++ src/Menu.java
@@ -4,9 +4,14 @@ import java.lang.String;
 public class Menu
 { 
 	public static void main(String args[] )throws IOException{ 
+		
 	BufferedReader in =new BufferedReader(new InputStreamReader(System.in)); 
 
 	int op;
+	int op2;
+	int op3;
+	
+	//Men˙ inicial
 	System.out.print("Menu principal\n" );
 	System.out.print("Seleccione la operaci√≥n ha realizar:\n" );
 	System.out.print("1.- Modificar base de datos\n" ); 
@@ -14,22 +19,25 @@ public class Menu
 	System.out.print("3.- Buscar en el historial\n" );
 	System.out.print("4.- Salir\n" );
 	
+	//Obtenemos la opciÛn seleccionada por el user
 	op=Integer.parseInt(in.readLine());
 
 	switch(op){ 
-	case 1: 
+	
+	case 1: //Modificar la base de datos
 		System.out.print("\nSeleccione la operaci√≥n de modificaci√≥n desada:\n" );
 		System.out.print("1.- A√±adir\n " );
 		System.out.print("2.- Actualizar\n" );
 		System.out.print("3.- Eliminar\n" );
 		System.out.print("4.- Limpiar historial\n" );
 		
-		int op2;
+		
 		op2=Integer.parseInt(in.readLine());
 		
 		switch(op2){
-		case 1:
-			int op3;
+		
+		case 1: //Modificar la base de datos - aÒadir
+			
 			System.out.print("\nSeleccione lo que desea a√±adir:\n" );
 			System.out.print("1.- Dato independiente\n" );
 			System.out.print("2.- Conjunto de datos(fichero adjunto)\n" );
@@ -37,44 +45,45 @@ public class Menu
 			op3=Integer.parseInt(in.readLine());
 			
 			switch(op3){
-			case 1:
+			
+			case 1: //Modificar la base de datos - aÒadir - dato independiente
 				System.out.print("\nIntroduce el dato independiente:\n" );
-				//leer dato que usuario a√±ade al sistema
+				//TODO leer dato que usuario a√±ade al sistema
 				break;
-			case 2:
+			
+			case 2: //Modificar la base de datos - aÒadir - conjunto de datos
 				System.out.print("\nAdjunta el fichero con el conjunto de datos:\n" );
-				//leer fichero que se quiere a√±adir
+				//TODO leer fichero que se quiere a√±adir
 				break;
 				//default:
 			}
-		case 2:
-			//actualizar el sistema con las modificaciones que pueda haber sufrido
+		case 2: //Modificar la base de datos - actualizar
+			//TODO actualizar el sistema con las modificaciones que pueda haber sufrido
 			break;
-		case 3:
-			//eliminar el dato que el usuario crea conveniente
+		case 3: //Modificar la base de datos - eliminar
+			//TODO eliminar el dato que el usuario crea conveniente
 			break;
-		case 4:
-			//limpiar historial de busqueda 
+		case 4: //Modificar la base de datos - limpiar historial
+			//TODO limpiar historial de busqueda 
 			break;
 		//default:
 		}
 	case 2://realizar consulta
 		System.out.print("1.- Elegir camino predeterminado\n" );
 		System.out.print("2.- Crear un camino nuevo\n" );
-		int op3;
 		op3=Integer.parseInt(in.readLine());
 		switch(op3){
-		case 1:
-			//usuario elige un camino de la lista de caminos predetermidos
+		case 1: //realizar consulta - camino predeterminado
+			//TODO usuario elige un camino de la lista de caminos predetermidos
 			break;
-		case 2:
-			//usuario crea su propio camino 
+		case 2: //realizar consulta - camino nuevo
+			//TODO usuario crea su propio camino 
 			break;
 		//default:
 		}
 	case 3://buscar en el historial
 		System.out.print("Elige el camino que quieres consultar del historial:\n" );
-		//lista de caminos guardados y usuario elige uno y lo consulta
+		//TODO lista de caminos guardados y usuario elige uno y lo consulta
 	case 4://salir del programa
 		System.exit(0); 
 	}
