a3e2ca5fd2682db0384da2bf635f27951b04decf
gnizamaeb2desuI <polcosta555@gmail.com>
2016-04-12 10:15:12 +0200

    Update LeerFichero.java

--- src/LeerFichero.java
+++ src/LeerFichero.java
@@ -48,33 +48,7 @@ public class LeerFichero {
 		return m;
 	}
 	
-	//Metodo para ver si se encuentra el codigo en el archivo y si lo encuentra en que linea está.
-	/*public static void buscaCodigo(String archivo, int codigo) throws FileNotFoundException, IOException {
-		String c = String.valueOf(codigo);
-		FileReader f = new FileReader(archivo);
-		BufferedReader b = new BufferedReader(f);
-		    int lineNum = 0;
-		    String codi;
-		    String currentLine; //= b.readLine();
-		    int trobat = -1;
-		    while(((currentLine = b.readLine()) != null) || trobat == 1)
-		    {
-		    	++lineNum;
-		    	int i = 0;
-		    	String copy = currentLine;
-		    	while(!(Character.isWhitespace(currentLine.charAt(i)))) {
-		    		++i;
-		    	}
-		    	codi = copy.substring(0,i);
-		    	if(codi.contentEquals(c)) {
-		    		System.out.printf("Codi trobat a la línia: %d\n",lineNum);
-		    		trobat = 1;
-		    	}
-		 
-		    }
-		    if (trobat != 1) System.out.printf("No existeix a l'arxiu");
-		    b.close();
-	}*/
+	
 	public static void printArray(ArrayList<Integer> a) throws  IOException {
 		Integer list2[] = new Integer[a.size()];
 		   list2 = a.toArray(list2);
