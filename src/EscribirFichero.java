df67d5bec81310c426929f3edb5705fc397a55ea
gnizamaeb2desuI <polcosta555@gmail.com>
2016-04-13 10:44:52 +0200

    Update EscribirFichero.java

--- src/EscribirFichero.java
+++ src/EscribirFichero.java
@@ -3,22 +3,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+//Aquesta classe simplement li passes un HashMap<Integer,<ArrayList<Integer>>> i et reescriu el document
 
 public class EscribirFichero {
 	
-	public static void main(String[] args){
+	//a == 1 és un fitxer on la primera en ordre és la clau i la segona les relacionades amb ella
+	//i si a == 0 és la inversa
+	public static void ReescribirFichero(HashMap<Integer,ArrayList<Integer>> n, Integer l){
 		try{
-			FileWriter fw = new FileWriter("/Users/USUARIO/Downloads/PROP/DBLP_four_area/prova.txt",false);
-			HashMap<Integer,ArrayList<Integer>> n = new HashMap<Integer,ArrayList<Integer>>();
-			ArrayList<Integer> r = new ArrayList<Integer>();
-			r.add(234);
-			r.add(111);
-			r.add(892);
-			int s = 90;
-			n.put(s, r);
-			for (int i = 0; i < n.size(); i++) {
-				for (int j = 0; j < r.size(); j++){
-					fw.write(s + "\t"+ r.get(j) + "\r\n");
+			FileWriter fw = new FileWriter("/Users/USUARIO/Downloads/PROP/DBLP_four_area/prova1.txt",false);
+		    Iterator<Entry<Integer, ArrayList<Integer>>> it = n.entrySet().iterator();
+		    while (it.hasNext()) {
+				Map.Entry<Integer, ArrayList<Integer>> e = it.next();
+				ArrayList<Integer> b = new ArrayList<Integer>();
+				int a = e.getKey();
+				b = e.getValue();
+				for (int j = 0; j < b.size(); j++){
+					if(l == 1) fw.write(a + "\t"+ b.get(j) + "\r\n");
+					else fw.write(b.get(j) + "\t"+ a + "\r\n");
 				}
 			}
 			fw.close();
@@ -26,4 +32,23 @@ public class EscribirFichero {
 			System.out.println("Error de:"+e);
 		}
 	}
+	
+	
+	public static void main(String[] args){
+		HashMap<Integer,ArrayList<Integer>> n = new HashMap<Integer,ArrayList<Integer>>();
+		ArrayList<Integer> r = new ArrayList<Integer>();
+		ArrayList<Integer> m = new ArrayList<Integer>();
+		r.add(234);
+		r.add(111);
+		r.add(892);
+		int s = 90;
+		n.put(s, r);
+		m.add(999);
+		m.add(231);
+		int c = 80;
+		n.put(c, r); 
+		int a = 0;
+		ReescribirFichero(n,a);
+	}
 }
+
