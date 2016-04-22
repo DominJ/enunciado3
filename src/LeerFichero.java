b934dee9a8816e7c116783910b1c84d456fd7fbc
gnizamaeb2desuI <polcosta555@gmail.com>
2016-04-22 17:06:54 +0200

    Update LeerFichero.java

--- src/LeerFichero.java
+++ src/LeerFichero.java
@@ -1,7 +1,7 @@
 package clasesCompartidas;
 
-import java.io.BufferedReader;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,37 +10,46 @@ import java.util.HashMap;
 
 
 
-public class LeerFichero {
+public class LeerFichero 
+{
 	
-	//Si tipusFitxer = 1, utilitzarem el tipusNode per saber la dada primitiva (0 = P, 1 = A, 2 = C, 3 = T)  
+	//C serveix per discriminar el tipus de relaciÃ³ amb paper, utilitzarem la c de la segÃ¼ent manera (1 = PA, 2 = PC, 3 = PT)  
 	
-	//La string archivo hay que escribirla con las barras no invertidas, sino te salta error
-	public static HashMap<Integer,ArrayList<Integer>> crear_relacion(int c) throws IOException {
+	//Aquest metode retorna un pair amb un HashMap original de relacions a la primera posiciÃ³ i un HashMap transposat a
+	// la segona posiciÃ³ de la relaciÃ³ de dos tipus de node
+	public static Pair<HashMap<Integer,ArrayList<Integer>>,HashMap<Integer,ArrayList<Integer>>> crear_relacion(int c) throws IOException {
 		String cadena;
-		//Este metodo lee el archivo
+		//Aquest metode llegeix el fitxer
+		Integer vb = 0;
 		String archivo = null;
-		if (c == 1) archivo = "/home2/users/alumnes/1193773/dades/DBLP_four_area/paper_author.txt";
-		if (c == 2) archivo = "/home2/users/alumnes/1193773/dades/DBLP_four_area/paper_conf.txt";
-		if (c == 3) archivo = "/home2/users/alumnes/1193773/dades/DBLP_four_area/paper_term.txt";
+		if (c == 1) archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/paper_author.txt";
+		else if (c == 2) archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/paper_conf.txt";
+		else archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/paper_term.txt";
 		FileReader f = new FileReader(archivo); 
 		BufferedReader b = new BufferedReader(f); 
 		HashMap<Integer,ArrayList<Integer>> m = new HashMap<Integer,ArrayList<Integer>>();
+		HashMap<Integer,ArrayList<Integer>> n = new HashMap<Integer,ArrayList<Integer>>();
 		Integer codi,codi2,codi_antic;
 		codi = codi2 = codi_antic = 0;
 		Boolean primer = true;
 		String s2 = new String();
 		ArrayList<Integer> l = new ArrayList<Integer>();
 		while(((cadena = b.readLine())!=null)){
+			++vb;
 	    	int i = 0;
 	    	String copy = cadena;
 	    	while(!(Character.isWhitespace(cadena.charAt(i)))) {
 	    		++i;
 	    	}
-	    	
+	    	System.out.println("ja hi som " + vb + "\n");
 	    	String s = copy.substring(0,i);
 	    	codi = Integer.parseInt(s);
 	    	String s1 = copy.substring(i+1,copy.length());
 	    	codi2 = Integer.parseInt(s1);
+	    	//Segon Hashmap del pair
+	    	if (n.get(codi2) == null) n.put(codi2, new ArrayList<Integer>());
+	    	n.get(codi2).add(codi);
+	    	//Primer HashMap del pair
 	    	if(primer){
 	    		s2 = copy.substring(0,i);
 	    		codi_antic = codi;
@@ -55,22 +64,29 @@ public class LeerFichero {
 	    	s2 = copy.substring(0,i);
 		}
 		m.put(codi_antic, l);
-		//Cerramos el buffer
+		//Tanquem el buffer
 		b.close();
-		return m;
+		//Omplim el pair amb els dos HashMaps obtinguts
+		Pair<HashMap<Integer,ArrayList<Integer>>,HashMap<Integer,ArrayList<Integer>>> v = new Pair<HashMap<Integer,ArrayList<Integer>>,HashMap<Integer,ArrayList<Integer>>>();
+		v.setFirst(m);
+		v.setSecond(n);
+		return v;
 	}
 
-	public static HashMap<Integer,Nodo> crear_nodo_primitivo(int c) throws IOException {
+	//Aquest mÃ¨tode retorna un pair amb un HashMap original de node primitiu a la primera posiciÃ³ i un HashMap transposat a
+	// la segona posiciÃ³. Els codis dels tipus sÃ³n (0 = P, 1 = A, 2 = C, 3 = T) 
+	public static Pair<HashMap<Integer,String>,HashMap<String,Integer>> crear_nodo_primitivo(int c) throws IOException {
 		String cadena;
-		//Este metodo lee el archivo
+		//Aquest mÃ¨tode llegeix el fitxer
 		String archivo = null;
-		if (c == 0) archivo = "/home2/users/alumnes/1193773/dades/DBLP_four_area/paper.txt";
-		else if (c == 1) archivo = "/home2/users/alumnes/1193773/dades/DBLP_four_area/author.txt";
-		else if (c == 2) archivo = "/home2/users/alumnes/1193773/dades/DBLP_four_area/conf.txt";
-		else archivo = "/home2/users/alumnes/1193773/dades/DBLP_four_area/term.txt";
+		if (c == 0) archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/paper.txt";
+		else if (c == 1) archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/author.txt";
+		else if (c == 2) archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/conf.txt";
+		else archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/term.txt";
 		FileReader f = new FileReader(archivo); 
 		BufferedReader b = new BufferedReader(f); 
-		HashMap<Integer,Nodo> m = new HashMap<Integer,Nodo>();
+		HashMap<Integer,String> m = new HashMap<Integer,String>();
+		HashMap<String,Integer> n = new HashMap<String,Integer>();
 		Integer codi = 0;
 		while(((cadena = b.readLine())!=null)){
 	    	int i = 0;
@@ -81,29 +97,17 @@ public class LeerFichero {
 	    	String s = copy.substring(0,i);
 	    	codi = Integer.parseInt(s);
 	    	String s1 = copy.substring(i+1,copy.length());
-	    	Nodo q = new Nodo(s1);
-	    	m.put(codi, q);
+	    	//IntroduÃ¯m els elements al HashMap primer
+	    	m.put(codi, s1);
+	        //IntroduÃ¯m els elements al HashMap segon ja invertits
+	    	n.put(s1,codi);
 		}
-		//Cerramos el buffer
+		//Tanquem el buffer
 		b.close();
-		return m;
+		//Omplim el pair amb els dos HashMaps obtinguts
+		Pair<HashMap<Integer,String>,HashMap<String,Integer>> v = new Pair<HashMap<Integer,String>,HashMap<String,Integer>>();
+		v.setFirst(m);
+		v.setSecond(n);
+		return v;
 	}
-
-	
-	
-	public static void main(String [] args) throws IOException {
-		
-		//HashMap<Integer,ArrayList<Integer>> r = crear_relacion(1);
-		HashMap<Integer,Nodo> s = crear_nodo_primitivo(0);
-		//ArrayList<Integer> m = new ArrayList<Integer>();
-		Nodo m = new Nodo();
-		m = s.get(7678);
-		System.out.println(m.consultar_nombre());
-		//m = r.get(7632);
-		 /* for (Integer number : m) {
-			   System.out.println("Number0 = " + number.);
-		} */
-		   
-	}
-
 }
